6d2c4211a5f57c868519ddfbd42d632e2a9f47c1
ygchbxm <1532074469@qq.com>
2023-11-09 00:25:57 +0800

    完善 判断题评测 组件

--- src/components/EvaluationFormForTrueOrFalseQuestion.vue
+++ src/components/EvaluationFormForTrueOrFalseQuestion.vue
@@ -1,12 +1,24 @@
 <script setup>
+import {ref} from "vue";
+
 defineProps({
   title: String,
   time: String,
-  nameGroups:Object
+  nameGroups: Object
 })
+
+const selectedOptions = ref({});
 
 function submitQuestionnaire() {
 }
+
+function selectedOption(group, index, key) {
+  Reflect.set(selectedOptions.value, index, {
+    isRealName: group.isRealName,
+    selectedValue: key,
+  })
+}
+
 </script>
 <template>
   <div class="eFFCq-form">
@@ -23,13 +35,23 @@ function submitQuestionnaire() {
         <ul class="eFFCq-ul">
           <li v-for="(group,index) in nameGroups" class="eFFCq-option">
             <div class="eFFCq-option-value">
-              <span>{{index.length>1?index+1:`0${index+1}`}}</span>
-              <div>{{group.names.join(' , ')}}</div>
+              <span>{{ index > 9 ? index + 1 : `0${index + 1}` }}</span>
+              <div>{{ group.names.join(' , ') }}</div>
             </div>
-            <el-radio-group v-model="group.isRealName" class="ml-4">
-              <el-radio label="0" size="large">group.value</el-radio>
-              <el-radio label="1" size="large">group.value</el-radio>
-            </el-radio-group>
+            <div class="eFFCq-option-radios">
+              <div v-for="(item,key) in group.option"
+                   @click="selectedOption(group,index,key)"
+                   :class="[{
+                     'eFFCq-option-radio':selectedOptions[index]?.selectedValue!==key||true},{
+                     'eFFCq-option-radio-selected':selectedOptions[index]?.selectedValue===key}]">
+                <div class="eFFCq-radio-icon">
+                  <div class="eFFCq-radio-icon-child"></div>
+                </div>
+                <div class="eFFCq-radio-label">
+                  <span>{{ item.value }}</span>
+                </div>
+              </div>
+            </div>
           </li>
         </ul>
       </div>
@@ -44,6 +66,7 @@ function submitQuestionnaire() {
   .eFFCq-form-content {
     width: 800px;
     margin: 0 auto;
+
     .eFFCq-form-header {
       width: 100%;
       display: flex;
@@ -91,17 +114,20 @@ function submitQuestionnaire() {
         }
       }
     }
-    .eFFCq-form-main{
+
+    .eFFCq-form-main {
       margin-top: 10px;
       border-top: 1px solid #EBEBEBFF;
-      .eFFCq-ul{
-        .eFFCq-option{
+
+      .eFFCq-ul {
+        .eFFCq-option {
           width: 800px;
           height: 118px;
           margin: 10px 0;
           list-style: none;
           border-bottom: 1px solid #EBEBEBFF;
-          .eFFCq-option-value{
+
+          .eFFCq-option-value {
             display: flex;
             align-items: center;
             height: 28px;
@@ -109,9 +135,83 @@ function submitQuestionnaire() {
             font-size: 14px;
             font-weight: 400;
             line-height: 28px;
-            span{
+
+            span {
               font-weight: 500;
               margin-right: 10px;
+            }
+          }
+
+          .eFFCq-option-radios {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+
+            .eFFCq-option-radio {
+              width: 100%;
+              height: 40px;
+              cursor: pointer;
+              display: flex;
+              align-items: center;
+              margin-top: 1px;
+
+              .eFFCq-radio-icon {
+                width: 16px;
+                height: 16px;
+                border-radius: 8px;
+                border: 1px solid #DCDCDCFF;
+                margin-left: 25px;
+                margin-right: 8px;
+              }
+
+              .eFFCq-radio-label {
+                line-height: 40px;
+                //text-align: center;
+              }
+            }
+
+            .eFFCq-option-radio:hover {
+              border-radius: 2px;
+              background: #e1f4f9;
+
+              .eFFCq-radio-icon {
+                background: #FFFFFF;
+              }
+            }
+
+            .eFFCq-option-radio-selected {
+              width: 100%;
+              height: 40px;
+              cursor: pointer;
+              display: flex;
+              align-items: center;
+              border-radius: 2px;
+              background: #e1f4f9;
+
+              .eFFCq-radio-icon {
+                width: 16px;
+                height: 16px;
+                border-radius: 8px;
+                border: 1px solid #00A9CEFF;
+                margin-left: 25px;
+                margin-right: 8px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: #FFFFFF;
+
+                .eFFCq-radio-icon-child {
+                  width: 8px;
+                  height: 8px;
+                  border-radius: 4px;
+                  background: #00A9CEFF;
+                }
+              }
+
+              .eFFCq-radio-label {
+                line-height: 40px;
+                //text-align: center;
+              }
             }
           }
         }
