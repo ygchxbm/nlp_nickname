37994ef21a0552700be22d52e3b5b48760c364f2
hugh <2401874452@qq.com>
2023-11-08 10:05:24 +0800

    在路由的白名单中新增作答评测界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ export const router = createRouter({
 NProgress.configure({ showSpinner: false });
 
 const store = useUserInfo(pinia);
-const whiteList = ["/401", "403", "404","/","/home","/release-evaluation/index","/preview-evaluation/index"];
+const whiteList = ["/401", "403", "404","/","/home","/release-evaluation/index","/preview-evaluation/index","/response-evaluation/choiceQuestion"];
 
 router.beforeEach(async (to, from, next) => {
 	NProgress.start();
