1d0a808c2baf24314aefd2d46d23616da712ea32
hugh <2401874452@qq.com>
2023-11-09 18:05:33 +0800

    增加 预览状态 公共组件

---
+++ src/components/previewBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+<template>
+  <div class="preview-main">
+    <span>预览状态</span>
+  </div>
+</template>
+<style lang="scss" scoped>
+.preview-main{
+  width: 100%;
+  height: 100%;
+  background: #DEF4F9FF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span{
+    color:#00A9CEFF;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+    margin-right: 110px;
+  }
+}
+</style>
