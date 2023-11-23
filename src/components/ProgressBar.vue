519b68e801f1a86ea9d4663d887ec246e15316eb
hugh <2401874452@qq.com>
2023-11-23 15:52:21 +0800

    优化进度条组件

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,19 +1,24 @@
 <script setup>
-defineProps({
+import {computed} from "vue";
+
+const props = defineProps({
   doneOptionNum: Number,
   allOptionNum: Number
 })
-
+const percentage = computed(() => {
+  return (props.doneOptionNum / props.allOptionNum) * 100 || 0
+})
 </script>
 <template>
   <div class="progress-main">
-    <el-progress stroke-width:6 color="#00A9CEFF" stroke-linecap="butt" :percentage="(doneOptionNum/allOptionNum)*100" text-inside="text-inside"/>
+    <el-progress stroke-width:6 color="#00A9CEFF" stroke-linecap="butt" :percentage="percentage" text-inside="text-inside"/>
     <div class="progress-num"><span>{{ `${doneOptionNum}/${allOptionNum}` }}</span></div>
   </div>
 </template>
 <style lang="scss" scoped>
 .progress-main {
   width: 100%;
+
   .el-progress {
     :deep(.el-progress-bar) .el-progress-bar__outer .el-progress-bar__inner .el-progress-bar__innerText {
       span {
